157f064e9e1f0f4b68c5d5221f44897ac958947b
yogaibnu <yogaibnudinataa@gmail.com>
2020-03-19 09:06:52 +0700

    create actionsTypes

--- src/Action/actionCreator.js
+++ src/Action/actionCreator.js
@@ -1,20 +1,20 @@
 import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from
-'./actionsTypes'
+    './actionsTypes'
 let TodoId = 2
 export const addTodo = text => ({
- type: ADD_TODO,
- id: TodoId++,
- text
+    type: ADD_TODO,
+    id: TodoId++,
+    text
 })
 export const deleteTodo = (id) => ({
- type: REMOVE_TODO,
- id: id
+    type: REMOVE_TODO,
+    id: id
 })
 export const toggleTodo = (id) => ({
- type: TOGGLE_TODO,
- id: id
+    type: TOGGLE_TODO,
+    id: id
 })
 export const setVisibilityFilter = filter => ({
- type: SET_VISIBILITY_FILTER,
- filter
+    type: SET_VISIBILITY_FILTER,
+    filter
 })
